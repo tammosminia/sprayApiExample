2edcf58af0652885e0868942400f11fea8392a62
tammosminia <tammosminia@gmail.com>
2015-04-06 13:19:44 +0200

    api with spray-can and spray-servlet

--- src/main/scala/SprayApiApp.scala
+++ src/main/scala/SprayApiApp.scala
@@ -3,18 +3,21 @@ import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
 import spray.can.Http
+import spray.servlet.WebBoot
 import scala.concurrent.duration._
 
-object SprayApiApp extends App {
-  //we need an ActorSystem to host our application in
+trait SprayApi {
   implicit val system = ActorSystem("SprayApiApp")
-
-  //create apiActor
   val apiActor = system.actorOf(Props[ApiActor], "apiActor")
+}
 
-  //timeout needs to be set as an implicit val for the ask method (?)
+//for use with spray-servlet
+class SprayApiServlet extends WebBoot with SprayApi {
+  override val serviceActor = apiActor
+}
+
+//for use with spray-can
+object SprayApiCan extends App with SprayApi {
   implicit val timeout = Timeout(5.seconds)
-
-  //start a new HTTP server on port 8080 with apiActor as the handler
   IO(Http) ? Http.Bind(apiActor, interface = "localhost", port = 8080)
 }
